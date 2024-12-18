a00f1926796d319a0e26b4da3a5c59907260eb28
Ross Peterson <petersonrl1@gmail.com>
2024-12-17 21:39:51 -0500

    add h1 and fix color contrast

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="mx-auto grid min-h-screen max-w-[900px] grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8">
+        <h1 className="font-bold">String Manipulation</h1>
         <div className="flex flex-col gap-6 text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <div>
             In the text area below, add a string delimited with parentheses and
